6dc17765f1d8d63c1546c292dbe76669109a5215
samruddhi <paisamruddhi4545@gmail.com>
2025-11-09 23:24:23 +0530

    files

--- backend_receiver.py
+++ backend_receiver.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 import uvicorn#type:ignore
 import json
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
 
 # Define the data structure the sensor/mobile will send
 class SensorSignal(BaseModel):
@@ -15,6 +16,18 @@ class SensorSignal(BaseModel):
 DATA_FILE = "signal.json"
 
 app = FastAPI()
+
+origins = [
+    "*",  # Allows all origins (including local files and remote hosts)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Must allow all methods (including OPTIONS, POST)
+    allow_headers=["*"], # Must allow all headers
+)
 
 @app.post("/api/sensor/signal")
 async def receive_signal(signal: SensorSignal):
@@ -44,3 +57,4 @@ if __name__ == "__main__":
     # Run the server (typically on a port like 8000)
     # You'll need to install: pip install fastapi uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8001)
+#HVAC-COND-R4,Critical failure detected: Refrigerant pressure drop and fan motor is vibrating severely. Requires low voltage control diagnostics.
